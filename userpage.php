2d00d0f352a4fbdcff1b590bba5c58785c8af9e1
vivekpatt <vivekrpatt@gmail.com>
2016-04-09 22:43:02 +0530

    userpage

--- userpage.php
+++ userpage.php
@@ -1,3 +1,19 @@
 <?php
 echo "login success";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<script src="bootstrap/js/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+
+  	
+  </body>
+</html>
