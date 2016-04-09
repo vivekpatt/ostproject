be500a48ec6917bbdc131694e103e07db81d2fe4
vivekpatt <vivekrpatt@gmail.com>
2016-04-09 20:51:59 +0530

    new

--- index.php
+++ index.php
@@ -19,13 +19,7 @@
    }
 
      if ( isset($_POST["uid_reg"]) && isset($_POST["email_reg"]) && isset($_POST["pwd_reg"]) ) {
-<<<<<<< HEAD
       // enters the block if register button is clicked.
-=======
-      
-      // enters the block if register button is clicked
-      
->>>>>>> origin/master
       // unset($_SESSION["account"]);  // Logout current user
       $_SESSION["error"] = "register";
       
